08bb72df738910cf0b4dec6768acdb4a164e423e
Adrian Tam <tam.ad@northeastern.edu>
2023-11-30 03:10:35 -0500

    environ

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
-const MODULES_URL = `https://kanbas-node-server-app-8t7d.onrender.com/api/modules`;
-const COURSES_URL = `https://kanbas-node-server-app-8t7d.onrender.com/api/courses`;
+const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = `${API_BASE}/courses`;
 
 // Exports /////////////////////////////////
 
